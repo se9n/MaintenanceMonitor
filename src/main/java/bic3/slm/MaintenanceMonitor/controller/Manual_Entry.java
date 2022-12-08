cfa7b368f4e2395042f24b4f598195c4054a1e80
avn7222 <ic21b112@technikum-wien.at>
2022-12-08 15:49:35 +0100

    requestbody done

---
+++ src/main/java/bic3/slm/MaintenanceMonitor/controller/Manual_Entry.java
@@ -0,0 +1,16 @@
+package bic3.slm.MaintenanceMonitor.controller;
+
+public class Manual_Entry {
+
+    String entry;
+
+    public void setMessage(String message) {
+        this.entry = message;
+    }
+
+    @Override
+    public String toString(){
+
+        return entry;
+    }
+}
