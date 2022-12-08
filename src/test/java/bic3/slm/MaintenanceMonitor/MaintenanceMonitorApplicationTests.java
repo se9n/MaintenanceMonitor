791dd2cc00566f81318e1ac87f7d79d8080d674b
avn7222 <ic21b112@technikum-wien.at>
2022-12-08 22:06:15 +0100

    unit tests

--- src/test/java/bic3/slm/MaintenanceMonitor/MaintenanceMonitorApplicationTests.java
+++ src/test/java/bic3/slm/MaintenanceMonitor/MaintenanceMonitorApplicationTests.java
@@ -2,25 +2,55 @@ package bic3.slm.MaintenanceMonitor;
 
 import bic3.slm.MaintenanceMonitor.controller.MaintenanceMonitorController;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Random;
 
 @SpringBootTest
 class MaintenanceMonitorApplicationTests {
 
+	String defaultm = "everything works as expected";
+
+	MaintenanceMonitorController generatortest = new MaintenanceMonitorController();
+
 	@Test
 	void MessageGenerated(){
 
-
-		String defaultm = "everything works as expected";
-
-		MaintenanceMonitorController generatortest = new MaintenanceMonitorController();
-		System.out.println(generatortest.currentStatus());
+		//System.out.println(generatortest.currentStatus());
 		Assertions.assertEquals(defaultm, generatortest.currentStatus());
 	}
 
-	void contextLoads() {
+
+	@Test
+	void ManualEntryDisplayedCorrectly() {
+
+		//method:   https://www.baeldung.com/java-random-string
+		byte[] arrlength = new byte[10];
+		new Random().nextBytes(arrlength);
+		String randString = new String(arrlength);
+
+
+		generatortest.updateStatus(randString);
+		Assertions.assertEquals(randString, generatortest.currentStatus());
+
+
+	}
+
+
+	@Test
+	void MessageResettoDefault(){
+
+		generatortest.resetStatus();
+		Assertions.assertEquals(defaultm, generatortest.currentStatus());
+
+	}
+
+
+	@Test
+	void contextLoads()
+	{
+
 	}
 
 
