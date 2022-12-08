854d502d277ec7ed900b775cf1a6ab52adfeac28
avn7222 <ic21b112@technikum-wien.at>
2022-12-08 17:40:43 +0100

    unit currentStatus draft

--- src/test/java/bic3/slm/MaintenanceMonitor/MaintenanceMonitorApplicationTests.java
+++ src/test/java/bic3/slm/MaintenanceMonitor/MaintenanceMonitorApplicationTests.java
@@ -1,6 +1,7 @@
 package bic3.slm.MaintenanceMonitor;
 
 import bic3.slm.MaintenanceMonitor.controller.MaintenanceMonitorController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +13,11 @@ class MaintenanceMonitorApplicationTests {
 	void MessageGenerated(){
 
 
+		String defaultm = "everything works as expected";
+
 		MaintenanceMonitorController generatortest = new MaintenanceMonitorController();
 		System.out.println(generatortest.currentStatus());
+		Assertions.assertEquals(defaultm, generatortest.currentStatus());
 	}
 
 	void contextLoads() {
