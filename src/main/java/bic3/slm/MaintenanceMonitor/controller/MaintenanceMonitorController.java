58194e23ac3b9b3757898f5c04c1887ece033e67
se9n <sean.caraet@gmail.com>
2022-12-09 20:35:15 +0100

    added descriptions to each method (instructions are in german due to the company being based in Austria ;))

--- src/main/java/bic3/slm/MaintenanceMonitor/controller/MaintenanceMonitorController.java
+++ src/main/java/bic3/slm/MaintenanceMonitor/controller/MaintenanceMonitorController.java
@@ -8,13 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MaintenanceMonitorController {
 
+    /*
+    *@param message Nachricht, die anzuzeigen ist, default ist "everything works as expected"
+    */
     String message = "everything works as expected";
 
+    /**
+     * Diese Methode gibt die aktuellste Statusnachricht message aus
+     *
+     *
+     * @return message, also aktuellste Statusnachricht
+     */
     @GetMapping("/api/message/")
     public String currentStatus() {
         return message;
     }
 
+    /**
+     * Diese Methode ermöglicht es uns die Statusnachricht message zu ändern. Dies kann manuell von
+     * einem User verändert werden, auf was er*sie möchte
+     * @param changeMessage die Nachricht, die dann als String message übernommen wird
+     * @return "ok", um dem User zu bestätigen, dass die Nachricht erfolgreich übermittelt wurde
+     */
     @PostMapping("/api/message/set")
     public String changeStatus(@RequestParam(value = "m") String changeMessage) {
 
@@ -23,6 +38,12 @@ public class MaintenanceMonitorController {
         return "ok";
     }
 
+    /**
+     * Diese Methode setzt String message zurück auf die default Nachricht "everything works as expected"
+     *
+     *
+     * @return "ok", um dem User zu bestätigen, dass die Nachricht erfolgreich übermittelt wurde
+     */
     @GetMapping("/api/message/reset")
     public String resetStatus() {
 
